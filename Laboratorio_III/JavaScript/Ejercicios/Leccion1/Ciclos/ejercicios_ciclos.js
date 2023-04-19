654705808fa9dc832a6e5e91e6b2d0cb70e2030f
Facundo Flores <facutomas1@gmail.com>
2023-04-18 22:29:41 -0300

    Update ejercicios_ciclos.js

--- Laboratorio_III/JavaScript/Ejercicios/Leccion1/Ciclos/ejercicios_ciclos.js
+++ Laboratorio_III/JavaScript/Ejercicios/Leccion1/Ciclos/ejercicios_ciclos.js
@@ -52,59 +52,73 @@ list.forEach(item => {
 console.log('El promedio es: ', prom/cont);
 console.log('La calificacion mas baja es: ', calLow)
 
-// Ejercicios de Facundo Tomas Flores
+
+// Ejercicios de Enzo Jesus Cruzate
 
 /*
-   1 - Diseñar un programa que en base a un numero aleatorio entre 0 y 2023, nos
-    devuelva por consola si es un año bisiesto o no, reperir esta accion 10 veces. 
+    1- Cree un programa en donde haya una lista de 5 numero, y calcule la cantidad de ceros, 
+    la media de los números positivos y la media de los números negativos en la lista.
 */
 
+const lista = []
+let cantCeros = 0;
+let sumPositivos = 0;
+let cantPositivos = 0;
+let sumNegativos = 0;
+let cantNegativos = 0;
 
-
-
-function Bisiesto(anioB){
-    if ((anioB % 4 === 0 && anioB % 100 !== 0 || anioB % 400 === 0)){
-        return true    
-    }
-     else {
-        return false
-     }
+for (let i=0; i<5;i++){
+    lista.push((Math.floor(Math.random()*201)-100));
 }
 
-for (i = 0; i < 10; i++) {
-    var anioB = Math.floor(Math.random() * 2024); 
-    if (Bisiesto(anioB)) {
-        console.log("El año"+anioB+ " es un año bisiesto.");
-    } else {
-        console.log("El año"+anioB+ " no es un año bisiesto.");
+for (let i=0; i<lista.length;i++){
+    if(lista[i] === 0){
+        cantCeros++;
+    }else if (lista[i]>0){
+        sumPositivos += lista[i];
+        cantPositivos++;
+    }else{
+        sumNegativos += lista[i];
+        cantNegativos++;
     }
-
 }
+
+let mediaPositivos = sumPositivos/cantPositivos;
+let mediaNegativos = sumNegativos/cantNegativos;
+
+console.log("La cantidad de ceros es: ",cantCeros)
+console.log("La media de los números positivos es: ",mediaPositivos);
+console.log("La media de los números negativos es: ",mediaNegativos);
 
 
 /*
-    2 - Suponga que se tiene un conjunto de calificaciones de un grupo de 10 alumnos. 
-    Realizar un algoritmo para calcular la calificacion promedio y la calificacion mas baja de todo el grupo.
+    2- Genere una lista de N° enteros, visualizar la suma de los números pares de la lista
+    cuantos números pares existen y cual es el promedio de los números impares.
 */
 
-const calificacionesA = [];
+const lista = []
+let sumPares = 0;
+let cantPares = 0;
+let cantImpares = 0;
+let sumImpares = 0;
 
-for (let i = 0; i < 10; i++) {
-    
-    calificacionesA.push(Math.floor(Math.random()*11));
-    
+for (let i=0;i<20;i++){
+    lista.push(Math.floor(Math.random()*100)+1);
 }
-console.log(calificacionesA)
 
-let sumCalificaciones = 0;
-let notaMenor = 10;
-
-calificacionesA.forEach(element => {
-    sumCalificaciones += element;
-    if (element < notaMenor) {
-        notaMenor = element;
+for (let i=0;i<lista.length;i++){
+    if(lista[i] % 2 == 0){
+        sumPares += lista[i];
+        cantPares++;
+    }else{
+        sumImpares += lista[i];
+        cantImpares++;
     }
-});
+}
 
-console.log("El promedio total es: ",sumCalificaciones / calificacionesA.length);
-console.log("La nota menor es: ",notaMenor);
+let promImpares = sumImpares/cantImpares;
+
+console.log("La cantidad de números pares es: ",cantPares);
+console.log("La suma de los números pares es: ",sumPares);
+console.log("La cantidad de los números impares es: ",cantImpares)
+console.log("El promedio de los números impares es: ",promImpares)
