9952f0bc7c7379a67478042d23bcba16e1704e7a
Facundo Flores <facutomas1@gmail.com>
2023-04-18 22:31:12 -0300

    Update ejercicios_ciclos.js
    Edición del archivo ejercicios_ciclos.js, adición de los ejercicios de Facundo Flores.

--- Laboratorio_III/JavaScript/Ejercicios/Leccion1/Ciclos/ejercicios_ciclos.js
+++ Laboratorio_III/JavaScript/Ejercicios/Leccion1/Ciclos/ejercicios_ciclos.js
@@ -122,3 +122,56 @@ console.log("La cantidad de números pares es: ",cantPares);
 console.log("La suma de los números pares es: ",sumPares);
 console.log("La cantidad de los números impares es: ",cantImpares)
 console.log("El promedio de los números impares es: ",promImpares)
+
+// Ejercicios de Facundo Tomas Flores
+
+/*
+   1 - Diseñar un programa que en base a un numero aleatorio entre 0 y 2023, nos
+    devuelva por consola si es un año bisiesto o no, reperir esta accion 10 veces. 
+*/
+
+function Bisiesto(anioB){
+    if ((anioB % 4 === 0 && anioB % 100 !== 0 || anioB % 400 === 0)){
+        return true    
+    }
+     else {
+        return false
+     }
+}
+
+for (i = 0; i < 10; i++) {
+    var anioB = Math.floor(Math.random() * 2024); 
+    if (Bisiesto(anioB)) {
+        console.log("El año"+anioB+ " es un año bisiesto.");
+    } else {
+        console.log("El año"+anioB+ " no es un año bisiesto.");
+    }
+
+}
+
+/*
+    2 - Suponga que se tiene un conjunto de calificaciones de un grupo de 10 alumnos. 
+    Realizar un algoritmo para calcular la calificacion promedio y la calificacion mas baja de todo el grupo.
+*/
+
+const calificacionesA = [];
+
+for (let i = 0; i < 10; i++) {
+    
+    calificacionesA.push(Math.floor(Math.random()*11));
+    
+}
+console.log(calificacionesA)
+
+let sumCalificaciones = 0;
+let notaMenor = 10;
+
+calificacionesA.forEach(element => {
+    sumCalificaciones += element;
+    if (element < notaMenor) {
+        notaMenor = element;
+    }
+});
+
+console.log("El promedio total es: ",sumCalificaciones / calificacionesA.length);
+console.log("La nota menor es: ",notaMenor);
