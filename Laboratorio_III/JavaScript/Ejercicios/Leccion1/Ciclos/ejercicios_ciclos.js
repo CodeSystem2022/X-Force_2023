4a0fba86e5ef56f469d9f02a6fcc3b0148f7fed1
Cristian C <111474153+CristianChosma@users.noreply.github.com>
2023-04-20 21:12:02 -0300

    Update ejercicios_ciclos.js
    Edición del archivo ejercicios_ciclos.js, adición de los ejercicios de Cristian Calzolari.

--- Laboratorio_III/JavaScript/Ejercicios/Leccion1/Ciclos/ejercicios_ciclos.js
+++ Laboratorio_III/JavaScript/Ejercicios/Leccion1/Ciclos/ejercicios_ciclos.js
@@ -248,3 +248,50 @@ for (let i = 0; i < valores.length; i++) {
   suma += valores[i];
 }
 console.log('La suma del total de los sueldos es de: $'+ suma);
+
+// Ejercicios de Cristian Calzolari
+
+/*
+    1- El objetivo es recorrer una lista de números y mostrar el cuadrado de cada uno de ellos.
+    Este proceso debe continuar hasta que se detecte un número negativo en la lista o hasta que se haya recorrido toda la lista.
+*/
+    
+//Generar lista de números
+let listaNum = [2, 3, 5, 8, 1, 10];
+//Iicializamos la variable
+let i = 0;
+//Recorremos la lista y calculamos el cuadrado de cada uno de los n´meros de la lista.
+while (i < listaNum.length && listaNum[i] >= 0) {
+  let cuadrado = listaNum[i] * listaNum[i];
+  console.log(cuadrado);
+  i++;
+}
+
+/* 
+    2- La tarea consiste en generar una lista de 40 números aleatorios y determinar si cada uno de ellos es positivo o negativo. 
+    Este proceso debe repetirse hasta que se detecte un 0 en la lista o hasta que se haya recorrido toda la lista.
+*/
+
+//  Generar una lista de 40 números aleatorios
+let numeros = [];
+for (let i = 0; i < 40; i++) {
+  numeros.push(Math.floor(Math.random() * 201) - 100);
+}
+//Inicializamos las variables
+let positivos = 0; let negativos = 0; let total = 0;
+// Recorrer la lista y determinar si el número es positivo o negativo
+for (let numero of numeros) {
+  if (numero === 0) {
+    console.log("Se detecto un 0");
+    break;
+  } else if (numero > 0) {
+    positivos++;
+  } else {
+    negativos++;
+  }
+  total++;
+}
+//Mostramos el resultado
+console.log('La cantidad de números eljidos al azar fueron: ', (total));
+console.log('Los números positivos son: ', (positivos));
+console.log('Los números negativos son: ', (negativos));
