2b15375f85d4ae1bb878aeb5637c9e7787888adf
JuanVillagran <107652522+JuanVillagran@users.noreply.github.com>
2023-06-08 00:21:20 -0300

    Update semana 05 Python
    Agrego los archivos de las tareas realizadas en python durante la segunda semana de mayo.
    se modifico el archivo prueba_db.py y se crearon los archivos; v_registros.py, v_i_registros.py, actualizar_registros.py, actualizar_varios_reg.py, delete_registro.py y delete_varios_reg.py

---
+++ Laboratorio_3/Python/Semana_5/BD/actualizar_registros.py
@@ -0,0 +1,16 @@
+import psycopg2
+
+conexion = psycopg2.connect( user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_bd')
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'#Placeholder
+            valores = ('Juan Carlos', 'Roldan', 'rcarlos@mail.com', 1) # es una tupla
+            cursor.execute(sentencia, valores) # ejecutamos la sentencia
+            registros_actualizados = cursor.rowcount
+            print(f'Los registros actualizados son: {registros_actualizados}')
+
+except Exception as e:
+    print(f'Ocurrio un error: {e}')
+finally:
+    conexion.close()
---
+++ Laboratorio_3/Python/Semana_5/BD/delet_varios_reg.py
@@ -0,0 +1,17 @@
+import psycopg2
+
+conexion = psycopg2.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_bd')
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'DELETE FROM persona WHERE id_persona IN %s'
+            entrada = input('Digite los numeros de los registro a eliminar: (separados por coma) ')
+            valores = (tuple(entrada.split(',')),)  # Es una tupla de tuplas
+            cursor.execute(sentencia, valores)  # ejecutamos la sentencia
+            registros_eliminados = cursor.rowcount
+            print(f'Los registros eliminados son: {registros_eliminados}')
+
+except Exception as e:
+    print(f'Ocurrio un error: {e}')
+finally:
+    conexion.close()
