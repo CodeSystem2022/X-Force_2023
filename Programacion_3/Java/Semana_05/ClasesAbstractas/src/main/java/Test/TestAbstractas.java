57233169049de2af055e6241a05048428c7ada7f
JuanVillagran <107652522+JuanVillagran@users.noreply.github.com>
2023-06-27 14:10:23 -0300

    Comit de la clase 5 de Programacion

---
+++ Programacion_3/Java/Semana_05/ClasesAbstractas/src/main/java/Test/TestAbstractas.java
@@ -0,0 +1,11 @@
+
+package Test;
+
+import domain.*;
+
+public class TestAbstractas {
+    public static void main(String[] args){
+        FiguraGeometrica figuraGeometrica = new Rectangulo("Rectangulo");
+        figuraGeometrica.dibujar();
+    }
+}
