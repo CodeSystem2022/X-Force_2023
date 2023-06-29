5edf517e7f593bc5555c8d59ecc862129f90fe79
jeroalvarez1 <jeroalvarez1@gmail.com>
2023-06-29 19:45:11 -0300

    fix(PROYECTO): Se crearon nuevas semanas y se borraron carpetas duplicadas.

--- Programacion_3/Java/Semana_8/Lección08/ManejoExcepciones1/src/excepciones/OperacionExcepcion.java
+++
@@ -1,9 +0,0 @@
-
-package excepciones;
-
-public class OperacionExcepcion extends RuntimeException{
-    //Constructor
-    public OperacionExcepcion(String mensaje){
-        super(mensaje);
-    }
-}
